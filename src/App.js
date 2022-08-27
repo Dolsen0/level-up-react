7e4af6e3dfda3c5bc1b448469ccbaddd05109121
dolsen0 <derek.olsen@gmail.com>
2022-08-26 20:08:43 -0400

    initialized firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Categories } from './components/Categories.jsx';
-import { Creative } from './components/Creative.jsx';
-import { Fun } from './components/Fun.jsx';
 import { MainScore } from './components/MainScore.jsx';
-import { Mental } from './components/Mental.jsx';
-import { Physical } from './components/Physical.jsx';
+
 
 function App() {
   return (
