856de2f1e61ed5cd9d6d34b4f70fb21fe2ac2aba
dolsen0 <derek.olsen@gmail.com>
2022-09-05 15:59:44 -0400

    added first name in navbar

---
+++ src/components/FirstName.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function FirstName(){
+    const [firstName, setFirstName] = useState('');
+
+
+    useEffect(()=>{
+        fetch('https://level-up-do.web.app/user')
+        .then(results => results.json())
+        .then(results => setFirstName(results.firstName))
+        .catch(err=>console.error(err))
+
+    },[])
+
+    return(
+        <p>{firstName}</p>
+    )
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { ButtonGroup } from "react-bootstrap";
 import  Container  from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar"
+import FirstName from "./FirstName.jsx";
 import LoginButton from "./Login.jsx";
 import SignupButton from "./SignupButton.jsx";
 
@@ -12,7 +13,7 @@ export default function NavBar(){
         <Navbar bg="light" className="navbar" sticky="top" >
             <Container>
                 <Navbar.Brand>Level Up</Navbar.Brand>
-                <Navbar.Brand>Name</Navbar.Brand>
+                <Navbar.Brand>{FirstName()}</Navbar.Brand>
                 <Navbar.Brand>{new Date().toLocaleDateString()}</Navbar.Brand>
                 <div className="navButtons">
                 <ButtonGroup>
